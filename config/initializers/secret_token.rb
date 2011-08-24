1b124a25bcfc121e16b33ad2b27048f263c122a2
ildus523 <ildus523@mail.ru>
2011-08-25 01:20:25 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Delphi::Application.config.secret_token = '1a0921baa2321bf56e42be3e209d9f5cada19143a2d25c0c4cc584d8bc4b351a504cc3e2de463b159672edac6c7307d4827842694bdae7db6d10fd56e2425a3e'
